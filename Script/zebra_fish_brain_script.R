e67bf0eec5af4502182e4a3458476f76bffe16ea
nguyenntkn <128745593+nguyenntkn@users.noreply.github.com>
2025-06-21 12:17:18 -0300

    Change some variable names

--- Script/zebra_fish_brain_script.R
+++ Script/zebra_fish_brain_script.R
@@ -16,75 +16,68 @@ zebrafish_brain_file_names <- list.files(file.path(work_dir, "Data/raw_counts/")
                                    full.names = TRUE)
 
 # Prepare a list of all data files
-temp_df_list = list()
+# Apparently it's more memory efficient to append to a list (then convert to df)
+# rather than directly appending it into a df?
+temp_count_df_list = list()
 
 # Read all files within list and append into list
 for (i in 1:length(zebrafish_brain_file_names)) {
   file_name = gsub(".multiple.count$", "", basename(zebrafish_brain_file_names[[i]]))
-  temp_df_list[[i]] <- read.table(zebrafish_brain_file_names[[i]], col.names = c("gene", file_name))
+  temp_count_df_list[[i]] <- read.table(zebrafish_brain_file_names[[i]], col.names = c("gene", file_name))
 }
 
 # Merge all files within the list to make a count_data_df
-count_data_df <- temp_df_list %>% reduce(full_join, by = 'gene')
+count_data_df <- temp_count_df_list %>% reduce(full_join, by = 'gene')
 
 # Export count data
 write.csv(count_data_df, file.path(work_dir, 'Data/zebrafish_brain_count_data.csv'), row.names = FALSE) 
 
-# Remove the list to save space
-rm(temp_df, temp_df_list)
+# Temp list is quite large, remove to save storage space
+rm(temp_count_df_list)
 
 
 # ====================== 4. Making Meta Data DF ================================
 # Prepare a list of meta data
-meta_df_list = list()
+temp_meta_df_list = list()
 
 # Read all files within list and append into the list
 for (i in 1:length(zebrafish_brain_file_names)) {
-  temp_file_name = gsub(".multiple.count$", "", basename(zebrafish_brain_file_names[[i]]))
+  temp_file_name = gsub("\\.multiple\\.count$", "", basename(zebrafish_brain_file_names[[i]]))
   file_components = strsplit(temp_file_name, "_")[[1]]
   
-  meta_df_list[[i]] <- data.frame(
-    sample_ID = basename(zebrafish_brain_file_names[[i]]),
+  temp_meta_df_list[[i]] <- data.frame(
+    sample_ID = temp_file_name,
     species = file_components[2],
     tissue = file_components[3],
-    time_month = file_components[4],
+    time = file_components[4],
+    time_unit = file_components[5],
     row.names = NULL,
     stringsAsFactors = FALSE
   )
 }
 
 # Convert list to dataframe
-meta_df_list <- do.call(rbind, meta_df_list)
+meta_data_df <- do.call(rbind, temp_meta_df_list)
+
+# Export meta data
+write.csv(meta_data_df, file.path(work_dir, 'Data/zebrafish_brain_meta_data.csv'))
+
+# Remove temp list to save space, may not be necessary if data is small
+rm(temp_meta_df_list)
+
+# ===================== 4. Normalizing Count Data ==============================
+# Not sure what normalizing method to use yet :(
 
 
 
-# ============== Meta Data ===================
-# file_names <- list.files("~/MultiSpeciesTranscriptomics/raw_counts")
-# 
-# metadata_list = list()
-# 
-# for (i in 1:length(file_names)) {
-#   file_components <- strsplit(file_names[i], '_')[[1]]
-#   
-#   ID = file_components[1]
-#   species = file_components[2]
-#   tissue = file_components[3]
-#   time = file_components[4]
-#   time_unit = file_components[5]
-#   
-#   # Append as a data frame row
-#   metadata_list[[i]] <- data.frame(
-#     file_name = file_names[i],
-#     ID = ID,
-#     species = species,
-#     tissue = tissue,
-#     time = time,
-#     time_unit = time_unit,
-#     stringsAsFactors = FALSE
-#   )
-# }
-# 
-# # Combine list into a full data frame
-# metadata_df <- do.call(rbind, metadata_list)
-# print(metadata_df)
+
+
+
+
+
+
+
+
+
+
 

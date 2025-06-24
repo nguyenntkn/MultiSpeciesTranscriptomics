d0a762b9db3987dbdc24a8b0aa1c32fa41e92ea6
nguyenntkn <128745593+nguyenntkn@users.noreply.github.com>
2025-06-24 19:40:44 -0300

    Make meta and count data files for both species

---
+++ Script/overall_exploratory.R
@@ -0,0 +1,125 @@
+
+# ============================== 1. Library ====================================
+library('tidyverse')
+
+
+# ========================== 2. Work directory =================================
+# Change the work directory appropriately
+work_dir = '/Users/nguyennguyen/Documents/Clinical Bioinfo/Analytic and Storytelling/MultiSpeciesTranscriptomics'
+setwd(work_dir)
+
+
+# ================== 3. Making Count Data DF & Meta Data DF ====================
+# NOTE: Still figuring out how to make naming scheme more "universal" or easily editable
+# in case we want to look at other species or tissue.
+
+# Get zebrafish files
+file_names <- list.files(file.path(work_dir, "Data/raw_counts/"),
+                                   pattern = "dre.*\\.multiple\\.count$",
+                                   full.names = TRUE)
+
+# Prepare a count data list and meta data list of all data files
+# Apparently it's more memory efficient to append to a list (then convert to df)
+# rather than directly appending it into a df?
+temp_count_df_list = list()
+temp_meta_df_list = list()
+
+
+# Read all files within list and append into list
+for (i in 1:length(file_names)) {
+  
+  # Shorten file names
+  temp_file_name = gsub("\\.multiple\\.count$", "", basename(file_names[[i]]))
+
+  # Append count data into count data list
+  temp_count_df_list[[i]] <- read.table(file_names[[i]], col.names = c("gene", temp_file_name))
+  
+  # Append meta data into list, formatted with appropriate information based on file names
+  file_components = strsplit(temp_file_name, "_")[[1]]
+  temp_meta_df_list[[i]] <- data.frame(
+    sample_ID = temp_file_name,
+    species = file_components[2],
+    tissue = file_components[3],
+    time = paste0(file_components[4], file_components[5]),
+    row.names = temp_file_name,
+    stringsAsFactors = FALSE
+  )
+}
+
+
+# Merge all files within the list to make a count_data_df
+zebrafish_count_data_df <- temp_count_df_list %>% 
+  purrr::reduce(full_join, by = 'gene') %>% 
+  column_to_rownames('gene')
+zebrafish_meta_data_df <- do.call(rbind, temp_meta_df_list)
+
+
+# Export count data and meta data
+write.csv(zebrafish_count_data_df, file.path(work_dir, 'Data/zebrafish_count_data.csv'), row.names = FALSE) 
+write.csv(zebrafish_meta_data_df, file.path(work_dir, 'Data/zebrafish_meta_data.csv'))
+
+
+# Temp list is quite large, remove to save storage space
+rm(temp_count_df_list, temp_meta_df_list)
+
+
+# ------------------------------------------------------------------------------
+# Now do the same thing for killifish
+# Get killifish files
+file_names <- list.files(file.path(work_dir, "Data/raw_counts/"),
+                                   pattern = "nfu.*\\.multiple\\.count$",
+                                   full.names = TRUE)
+
+# Prepare a count data list and meta data list of all data files
+temp_count_df_list = list()
+temp_meta_df_list = list()
+
+
+# Read all files within list and append into list
+for (i in 1:length(file_names)) {
+  
+  # Shorten file names
+  temp_file_name = gsub("_mzm_tophat2_mapped\\.bam\\.multiple\\.count$", "", basename(file_names[[i]]))
+  
+  # Append count data into count data list
+  temp_count_df_list[[i]] <- read.table(file_names[[i]], col.names = c("gene", temp_file_name))
+  
+  # Append meta data into list, formatted with appropriate information based on file names
+  file_components = strsplit(temp_file_name, "_")[[1]]
+  temp_meta_df_list[[i]] <- data.frame(
+    sample_ID = temp_file_name,
+    species = file_components[2],
+    tissue = file_components[3],
+    time = paste0(file_components[4], file_components[5]),
+    row.names = temp_file_name,
+    stringsAsFactors = FALSE
+  )
+}
+
+
+# Merge all files within the list to make a count_data_df
+killifish_count_data_df <- temp_count_df_list %>% 
+  purrr::reduce(full_join, by = 'gene') %>% 
+  column_to_rownames('gene')
+killifish_meta_data_df <- do.call(rbind, temp_meta_df_list)
+
+
+# Export count data and meta data
+write.csv(killifish_count_data_df, file.path(work_dir, 'Data/killifish_count_data.csv'), row.names = FALSE) 
+write.csv(killifish_meta_data_df, file.path(work_dir, 'Data/killifish_meta_data.csv'))
+
+
+# Temp list is quite large, remove to save storage space
+rm(temp_count_df_list, temp_meta_df_list)
+
+
+
+
+
+
+
+
+
+
+
+

b40cb5bd406f62edc1e6e21fa356e503f9b8b9c5
nguyenntkn <128745593+nguyenntkn@users.noreply.github.com>
2025-06-24 18:22:52 -0300

    Incorporated Alejandro's code

--- Script/zebra_fish_brain_script.R
+++ Script/zebra_fish_brain_script.R
@@ -1,5 +1,11 @@
 # ============================== 1. Library ====================================
 library('tidyverse')
+library('DESeq2')
+library('pheatmap')
+library('msigdbr')
+library('fgsea')
+library('ggplot2')
+library('dplyr')
 
 # ========================== 2. Work directory =================================
 # Change the work directory appropriately
@@ -12,8 +18,8 @@ setwd(work_dir)
 
 # Get zebrafish brain files
 zebrafish_brain_file_names <- list.files(file.path(work_dir, "Data/raw_counts/"),
-                                   pattern = "dre_brain_.*\\.multiple\\.count$",
-                                   full.names = TRUE)
+                                         pattern = "dre_brain_.*\\.multiple\\.count$",
+                                         full.names = TRUE)
 
 # Prepare a list of all data files
 # Apparently it's more memory efficient to append to a list (then convert to df)
@@ -27,7 +33,9 @@ for (i in 1:length(zebrafish_brain_file_names)) {
 }
 
 # Merge all files within the list to make a count_data_df
-count_data_df <- temp_count_df_list %>% reduce(full_join, by = 'gene')
+count_data_df <- temp_count_df_list %>% 
+  purrr::reduce(full_join, by = 'gene') %>% 
+  column_to_rownames('gene')
 
 # Export count data
 write.csv(count_data_df, file.path(work_dir, 'Data/zebrafish_brain_count_data.csv'), row.names = FALSE) 
@@ -58,6 +66,7 @@ for (i in 1:length(zebrafish_brain_file_names)) {
 
 # Convert list to dataframe
 meta_data_df <- do.call(rbind, temp_meta_df_list)
+rownames(meta_data_df) <- meta_data_df$sample_ID
 
 # Export meta data
 write.csv(meta_data_df, file.path(work_dir, 'Data/zebrafish_brain_meta_data.csv'))
@@ -65,19 +74,100 @@ write.csv(meta_data_df, file.path(work_dir, 'Data/zebrafish_brain_meta_data.csv'
 # Remove temp list to save space, may not be necessary if data is small
 rm(temp_meta_df_list)
 
-# ===================== 4. Normalizing Count Data ==============================
-# Not sure what normalizing method to use yet :(
+# ======================== 5. DESeq2 analysis ==================================
+
+# Make DESeqDataSet object for DESeq
+dds <- DESeqDataSetFromMatrix(
+  countData = count_data_df,
+  colData = meta_data_df,
+  design = ~ time)
+
+# Perform DEA
+dds <- DESeq(dds)
+res <- results(dds)
+
+# Volcano plot of DESeq2 results
+res_df <- as.data.frame(res) %>% # matrix out of results
+  rownames_to_column("gene") %>%
+  mutate(significant = padj < 0.03)
+
+# volcano plot
+# x - >0 upregulated, <0 downregulated
+# y - statistical significance
+ggplot(res_df, aes(x = log2FoldChange, y = -log10(padj), color = significant, shape = significant)) +
+  geom_point(alpha = 0.6) + 
+  xlim(c(-3, 3)) + 
+  labs(x = "Log2 Fold Change", y = "-Log10 Adjusted P-value", title = "Volcano Plot") +
+  theme_minimal()
+
+perform_DESeq2 <- function(dds, condition1, condition2) {
+  res <- results(dds, contrast=c("time", condition1, condition2))
+  res_df <- as.data.frame(res) %>% # converts to data frame
+    rownames_to_column("gene") %>% # adds gene names
+    # adds column with T/F if gene is statistically significant
+    mutate(significant = padj < 0.05, 
+           condition = ifelse(log2FoldChange > 0, # gene upregulated
+                              condition1, # in condition 1 (a gene in one condition)
+                              condition2)) # otherwise condition 2 (a gene in other condition)
+  res_df <- na.omit(res_df) # removes incomplete genes
+  # volcano plot
+  ggplot(res_df, aes(x=log2FoldChange, y=-log10(padj), color=significant, shape=condition)) +
+    geom_point(alpha=0.7, size=2) +
+    scale_color_manual(values = c("turquoise", "red")) +
+    scale_shape_manual(values = c(16, 17)) +
+    labs(title = paste("Volcano Plot:", condition1, "vs", condition2),
+         x = "Log2 Fold Change", y = "-Log10 Adjusted p-value") +
+    theme_minimal() +
+    theme(legend.position = "top")
+}
+
+# Plot volcano plots for each condition pair
+plot1 <- perform_DESeq2(dds, "12", "24")
+plot2 <- perform_DESeq2(dds, "12", "36")
+plot3 <- perform_DESeq2(dds, "12", "42")
+
+plot1
+plot2
+plot3
 
 
+# =============== 6. GSEA - Gene Set Enrichment Analysis =======================
 
+### Load gene sets for GSEA
+# MSigDB = Molecular Signatures Database
+# Curated collection of gene sets used for GSEA
+zebrafish_gene_sets <- msigdbr(species = "Danio rerio", 
+                               category = "C5") # C5 - gene ontology terms
+# result: df from gene names and ensembl gene IDs
 
+# converts df to named list to use in fgsea()
+# Each list element = vector of genes for one gene set
+gene_sets_list <- split(zebrafish_gene_sets$ensembl_gene, 
+                        zebrafish_gene_sets$gs_name)
 
+### Rank genes for GSEA analysis
 
+genes_ranked <- -log10(res$pvalue) * sign(res$log2FoldChange)
+names(genes_ranked) <- rownames(res)
+genes_ranked <- genes_ranked[!is.na(genes_ranked) & is.finite(genes_ranked)]
 
+### Perform GSEA
+set.seed(42)
+fgseaRes <- fgsea(pathways = gene_sets_list, 
+                  stats = genes_ranked,
+                  minSize = 15, # min no of genes a set should have to be tested
+                  maxSize = 500)
 
+head(fgseaRes[order(pval), ])
 
+# Enrichment plot for a single pathway
+plotEnrichment(gene_sets_list[["GOCC_MITOCHONDRIAL_PROTEIN_CONTAINING_COMPLEX"]],
+               genes_ranked) + labs(title="GOCC_MITOCHONDRIAL_PROTEIN_CONTAINING_COMPLEX")
 
-
-
-
+# Enrichment plot for several pathways (Top 10 pathways up vs Top 10 pathways down)
+topPathwaysUp <- fgseaRes[ES > 0][head(order(pval), n=10), pathway]
+topPathwaysDown <- fgseaRes[ES < 0][head(order(pval), n=10), pathway]
+topPathways <- c(topPathwaysUp, rev(topPathwaysDown))
+plotGseaTable(gene_sets_list[topPathways], genes_ranked, fgseaRes, 
+              gseaParam=0.5)
 
